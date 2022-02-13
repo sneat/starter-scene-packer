9ff554b73ed07a41875ebc3a0977067702b3f37e
Blair McMillan <blair@mcmillan.id.au>
2022-02-14 09:41:46 +1000

    Adjust to using additionalModulePacks by default

--- scripts/init.js
+++ scripts/init.js
@@ -27,47 +27,44 @@
   /**
    * creaturePacks is a list of compendium packs to look in for Actors by name (in prioritised order).
    * If the creature is not found in the first pack, it will search through each subsequent pack.
-   * The first entry here assumes that you have an Actor pack in your module with the "name" of "actors".
-   * The second entry here lists the DnD5e SRD Monsters compendium that comes with the DnD5e system. Feel
-   * free to delete the 'dnd5e.monsters' reference if you want, or swapping it to any other system compendium
-   * reference.
    * Set to the following to disable:
    *   const creaturePacks = [];
    */
-  const creaturePacks = [`${moduleName}.actors`, 'dnd5e.monsters'];
+  const creaturePacks = [];
   /**
    * journalPacks is a list of compendium packs to look in for Journals by name (in prioritised order).
-   * The first entry here assumes that you have a Journal pack in your module with the "name" of "journals".
    * Set to the following to disable:
    *   const journalPacks = [];
    */
-  const journalPacks = [`${moduleName}.journals`];
+  const journalPacks = [];
   /**
    * macroPacks is a list of compendium packs to look in for Macros by name (in prioritised order).
-   * The first entry here assumes that you have a Macro pack in your module with the "name" of "macros".
    * Set to the following to disable:
    *   const macroPacks = [];
    */
-  const macroPacks = [`${moduleName}.macros`];
+  const macroPacks = [];
   /**
    * playlistPacks is a list of compendium packs to look in for Playlists by name (in prioritised order).
-   * The first entry here assumes that you have a Playlist pack in your module with the "name" of "playlists".
    * Set to the following to disable:
    *   const playlistPacks = [];
    */
-  const playlistPacks = [`${moduleName}.playlists`];
+  const playlistPacks = [];
   /**
    * additionalModulePacks is a list of modules whose compendium packs can be looked in for entities by name (in prioritised order).
    * Enter just the "name" of the module/s or system/s you want to be considered.
+   *
+   * By default, all packs within your module will be included along with those from the dnd5e system.
+   *
    * For example, if you wanted to allow the packs from
    * the [Free Loot Tavern's Magic Item Vault]{@link https://foundryvtt.com/packages/free-loot-tavern-compendium}
    * and [Dynamic Active Effects SRD]{@link https://foundryvtt.com/packages/Dynamic-Effects-SRD}
    * modules to be searchable:
    *   const additionalModulePacks= ['free-loot-tavern-compendium', 'Dynamic-Effects-SRD'];
+   *
    * Set to the following to disable:
    *   const additionalModulePacks = [];
    */
-  const additionalModulePacks = ['dnd5e'];
+  const additionalModulePacks = [moduleName, 'dnd5e'];
 
   Hooks.once('scenePackerReady', ScenePacker => {
     // Initialise the Scene Packer with your adventure name and module name
